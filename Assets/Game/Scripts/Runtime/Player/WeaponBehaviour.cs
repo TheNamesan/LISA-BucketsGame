e3c72282459fc15c0b0ad1113a01e94aa3a7398a
TheNamesan <thenamesan@gmail.com>
2024-01-26 23:31:41 -0600

    WIP Shotgun Behaviour

--- Assets/Game/Scripts/Runtime/Player/WeaponBehaviour.cs
+++ Assets/Game/Scripts/Runtime/Player/WeaponBehaviour.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 namespace BucketsGame
 {
+    public enum WeaponMode
+    {
+        Pistols = 0,
+        Shotgun = 1
+    }
     public class WeaponBehaviour : MonoBehaviour
     {
         [Header("Shoot")]
         public Bullet bulletPrefab;
+        public WeaponMode weaponMode = WeaponMode.Pistols;
         public int ticksFireRate = 10;
         private int m_ticks = 0;
 
@@ -20,11 +26,42 @@ namespace BucketsGame
         {
             if (!bulletPrefab) return;
             if (m_ticks > 0) return;
-            // Change this to a pool
-            var newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-            newBullet.Fire(normal);
-            m_ticks = ticksFireRate;
+            
+            if (weaponMode == WeaponMode.Pistols)
+            {
+                Vector3 offset = new Vector3(0, 0.1f, 0);
+                // Change this to a pool
+                SpawnBullet(normal);
+            }
+            else if (weaponMode == WeaponMode.Shotgun)
+            {
+                float bullets = 8;
+                float spreadAngle = 30f;
+                for (int i = 0; i < bullets + 1; i++)
+                {
+                    float a = -spreadAngle * 0.5f;
+                    float angle = -Vector2.SignedAngle(normal, Vector2.right) + Mathf.Lerp(-a, a, Mathf.InverseLerp(0, bullets + 1, i));
+                    float rad = Mathf.Deg2Rad * angle;
+                    Vector2 nor = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad));
+                    Vector2 dir = (nor).normalized;
+                    Debug.DrawRay(transform.position, dir, Color.white, 1f);
+                    
+                    SpawnBullet(dir);
+                }
+            }
+            m_ticks = GetFireRate();
             Debug.Log("Pew");
+        }
+
+        private void SpawnBullet(Vector2 dir) // Change this to a pool
+        {
+            var newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            newBullet.Fire(dir);
+        }
+
+        public int GetFireRate()
+        {
+            return ticksFireRate;
         }
     }
 }
