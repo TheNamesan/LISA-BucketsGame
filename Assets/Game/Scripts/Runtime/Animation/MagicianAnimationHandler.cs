2033f4ffd352b636874687f077615caaae6c542e
TheNamesan <thenamesan@gmail.com>
2024-03-30 18:44:25 -0600

    More magician sprite stuff

--- Assets/Game/Scripts/Runtime/Animation/MagicianAnimationHandler.cs
+++ Assets/Game/Scripts/Runtime/Animation/MagicianAnimationHandler.cs
@@ -7,5 +7,33 @@ namespace BucketsGame
     public class MagicianAnimationHandler : AnimationHandler
     {
         public Magician target;
+        private void Update()
+        {
+            ChangeAnimationState();
+        }
+        public void ChangeAnimationState(bool forcePlaySameState = false)
+        {
+            if (target == null) return;
+            if (!anim) return;
+
+            string stateName = GetAnimationStateName();
+            if (lastStateName == stateName && !forcePlaySameState) { return; }
+            anim.enabled = true;
+            anim.Play(stateName, -1, 0);
+            lastStateName = stateName;
+        }
+        public string GetAnimationStateName()
+        {
+            //if (target.dead)
+            //{
+            //    if (!target.grounded) return "FlyerDeadAir";
+            //    return "FlyerDead";
+            //}
+            //else if (target.firing)
+            //{
+            //    return "FlyerAttack";
+            //}
+            return "MagicianIdle";
+        }
     }
 }
