afebb1839efeebb25f930060038cd7c4e18752a4
TheNamesan <thenamesan@gmail.com>
2024-11-16 18:50:17 -0600

    Fixed OptionsMenu not including Buckets Game options

--- Assets/Game/TUFF/Scripts/Runtime/UI/Menus/OptionsMenuManager.cs
+++ Assets/Game/TUFF/Scripts/Runtime/UI/Menus/OptionsMenuManager.cs
@@ -1,4 +1,164 @@
+
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TUFF
+{
+    public class OptionsMenuManager : MonoBehaviour
+    {
+        [Header("References")]
+        public UIMenu uiMenu;
+        public UIPicker refreshRate;
+        public UIPicker windowMode;
+        public UIPicker windowRes;
+        public UISlider musicSlider;
+        public UISlider sfxSlider;
+        public UISlider ambienceSlider;
+        public UIPicker textSpeed;
+        public UIPicker autoFire;
+        public UIPicker swapDash;
+        public UIPicker toggleSlowmo;
+        public UIPicker aimAutoFire;
+
+        public void OpenOptionsMenu()
+        {
+            uiMenu.OpenMenu();
+        }
+        public void SetOptionsValues() // Called from OptionsMenu OnOpenMenu
+        {
+            if (GameManager.instance == null) return;
+            ConfigData configData = GameManager.instance.configData;
+            musicSlider.fillAmount = configData.globalMusicVolume * 100;
+            sfxSlider.fillAmount = configData.globalSFXVolume * 100;
+            ambienceSlider.fillAmount = configData.globalAmbienceVolume * 100;
+            windowMode.highlightedOption = configData.fullscreen ? 1 : 0;
+            windowMode.UpdateText();
+
+            Resolution currentResolution = new Resolution() { width = configData.resolutionWidth, height = configData.resolutionHeight, refreshRate = configData.refreshRate };
+
+            // Refresh Rate
+            refreshRate.options = new List<string>();
+            var refreshRates = GameManager.instance.frameRates;
+            for (int i = 0; i < refreshRates.Length; i++)
+            {
+                refreshRate.options.Add(LISAUtility.IntToString(refreshRates[i]));
+            }
+            int frameIndex = System.Array.IndexOf(refreshRates, configData.refreshRate);
+            if (frameIndex < 0) frameIndex = 0;
+            refreshRate.highlightedOption = frameIndex;
+            refreshRate.UpdateText();
+
+            // Resolution
+            windowRes.options = new List<string>();
+            var supportedResolutions = GameManager.instance.supportedResolutions;
+            int index = -1;
+            index = System.Array.FindIndex(supportedResolutions,
+                e => currentResolution.width == e.width && currentResolution.height == e.height);
+            if (index < 0)
+            {
+                GameManager.instance.ExpandSupportedResolutions(currentResolution);
+                supportedResolutions = GameManager.instance.supportedResolutions;
+                index = supportedResolutions.Length - 1;
+            }
+            for (int i = 0; i < supportedResolutions.Length; i++)
+            {
+                windowRes.options.Add(supportedResolutions[i].width + " x " + supportedResolutions[i].height);
+            }
+            windowRes.highlightedOption = index;
+            windowRes.UpdateText();
+
+            // Text Speed
+            textSpeed.highlightedOption = GameManager.instance.configData.textSpeed;
+            textSpeed.UpdateText();
+
+            autoFire.highlightedOption = GameManager.instance.configData.bucketsAutoFire ? 1 : 0;
+            autoFire.UpdateText();
+
+            //swapDash.highlightedOption = GameManager.instance.configData.bucketsSwapDash ? 1 : 0;
+            //swapDash.UpdateText();
+
+            toggleSlowmo.highlightedOption = ConfigData.instance.bucketsToggleSlowmo ? 1 : 0;
+            toggleSlowmo.UpdateText();
+
+            aimAutoFire.highlightedOption = ConfigData.instance.bucketsAimAutoFire ? 1 : 0;
+            aimAutoFire.UpdateText();
+        }
+
+        public void UpdateGlobalMusicVolume(float volume)
+        {
+            GameManager.instance.configData.globalMusicVolume = volume / 100;
+            GameManager.instance.UpdateGlobalVolume();
+        }
+
+        public void UpdateGlobalSFXVolume(float volume)
+        {
+            GameManager.instance.configData.globalSFXVolume = volume / 100;
+            GameManager.instance.UpdateGlobalVolume();
+        }
+
+        public void UpdateGlobalAMBSVolume(float volume)
+        {
+            GameManager.instance.configData.globalAmbienceVolume = volume / 100;
+            GameManager.instance.UpdateGlobalVolume();
+        }
+
+        public void SetFullscreen()
+        {
+            //Debug.Log("Updated Fullscreen:" + System.Convert.ToBoolean(windowMode.highlightedOption));
+            GameManager.instance.configData.fullscreen = System.Convert.ToBoolean(windowMode.highlightedOption);
+            GameManager.instance.SetGameFullscreen(GameManager.instance.configData.fullscreen);
+            SaveOptionsData();
+        }
+
+        public void SetResolution()
+        {
+            var res = GameManager.instance.supportedResolutions[windowRes.highlightedOption];
+            var frameRate = GameManager.instance.frameRates[refreshRate.highlightedOption];
+            GameManager.instance.configData.resolutionWidth = res.width;
+            GameManager.instance.configData.resolutionHeight = res.height;
+            GameManager.instance.configData.refreshRate = frameRate;
+            GameManager.instance.SetGameResolution(res.width, res.height, frameRate);
+            SaveOptionsData();
+        }
+
+        public void SetTextSpeed()
+        {
+            GameManager.instance.configData.textSpeed = textSpeed.highlightedOption;
+            SaveOptionsData();
+        }
+        public void SetBucketsAutoFire()
+        {
+            Debug.Log("Updated Auto Fire: " + autoFire.highlightedOption);
+            GameManager.instance.configData.bucketsAutoFire = System.Convert.ToBoolean(autoFire.highlightedOption);
+            SaveOptionsData();
+        }
+        public void SetBucketsSwapDash()
+        {
+            //Debug.Log("Updated Swap Dash: " + swapDash.highlightedOption);
+            //GameManager.instance.configData.bucketsSwapDash = System.Convert.ToBoolean(swapDash.highlightedOption);
+            //SaveOptionsData();
+        }
+        public void SetBucketsToggleSlowmo()
+        {
+            Debug.Log("Updated Toggle Slow Mo: " + toggleSlowmo.highlightedOption);
+            GameManager.instance.configData.bucketsToggleSlowmo = System.Convert.ToBoolean(toggleSlowmo.highlightedOption);
+            SaveOptionsData();
+        }
+        public void SetBucketsAimAutoFire()
+        {
+            Debug.Log("Updated Aim Auto Fire: " + aimAutoFire.highlightedOption);
+            GameManager.instance.configData.bucketsAimAutoFire = System.Convert.ToBoolean(aimAutoFire.highlightedOption);
+            SaveOptionsData();
+        }
+
+        public void SaveOptionsData()
+        {
+            GameManager.instance.configData.SaveData();
+        }
+    }
+}
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -116,3 +276,4 @@ namespace TUFF
         }
     }
 }
+*/
